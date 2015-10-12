531a84c37be978e8a00b1814785f9e3e1812a0ea
Manuel Rivero+Alvaro Garcia <trikitrok+derecerca@gmail.com>
2015-10-12 22:30:59 +0200

    refactor: changed test descriptions to remove reference to pressure

--- spec/alarm_spec.rb
+++ spec/alarm_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/alarm'
 
 describe Alarm do
-  it "is on when pressure is too low" do
+  it "is on when sampled value is too low" do
     alarm = alarm_with_sensor_sampling(15)
 
     alarm.check
@@ -9,7 +9,7 @@ describe Alarm do
     expect(alarm.alarm_on).to be_truthy
   end
 
-  it "is off when pressure is in the safety range" do
+  it "is off when sampled value is in the safety range" do
     alarm = alarm_with_sensor_sampling(18)
 
     alarm.check
@@ -17,7 +17,7 @@ describe Alarm do
     expect(alarm.alarm_on).to be_falsey
   end
 
-  it "is on when pressure is too high" do
+  it "is on when sampled value is too high" do
     alarm = alarm_with_sensor_sampling(22)
 
     alarm.check
