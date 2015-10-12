9e97e9de7c9380ce1278f4b489924db58b76b5ed
Manuel Rivero+Alvaro Garcia <trikitrok+derecerca@gmail.com>
2015-10-12 20:05:19 +0200

    green: the alarm should be off when it is in the safety range

--- spec/alarm_spec.rb
+++ spec/alarm_spec.rb
@@ -2,16 +2,29 @@ require_relative '../lib/alarm'
 
 describe Alarm do
   it "is on when pressure is too low" do
-    alarm = FakeAlarm.new()
+    alarm = FakeAlarm.new(15)
 
     alarm.check
 
     expect(alarm.alarm_on).to be_truthy
   end
+
+  it "is off when pressure is in the safety range" do
+    alarm = FakeAlarm.new(18)
+
+    alarm.check
+
+    expect(alarm.alarm_on).to be_falsey
+  end
+  
 end
 
 class FakeAlarm < Alarm
+  def initialize value
+    @value = value
+  end
+
   def sample_pressure
-    15
+    @value
   end
 end
