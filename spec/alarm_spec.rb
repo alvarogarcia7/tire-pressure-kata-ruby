1bbfb8b6786753b43dbb365a515f9c639b3940c0
Manuel Rivero+Alvaro Garcia <trikitrok+derecerca@gmail.com>
2015-10-12 19:45:53 +0200

    green: test the Alarm is on when pressure is too low
    monkey-patching to inject the sensor
    taking advantage of duck typing to use a fake Sensor to be able to test

--- spec/alarm_spec.rb
+++ spec/alarm_spec.rb
@@ -1,11 +1,28 @@
 require_relative '../lib/alarm'
 
 describe Alarm do
-  it "is on" do
+  it "is on when pressure is too low" do
     alarm = Alarm.new
+    alarm.set_sensor(FakeSensor.new(15))
 
     alarm.check
 
     expect(alarm.alarm_on).to be_truthy
+  end
+end
+
+class Alarm
+  def set_sensor sensor
+    @sensor = sensor
+  end
+end
+
+class FakeSensor
+  def initialize value
+    @value = value
+  end
+
+  def pop_next_pressure_psi_value
+    @value
   end
 end
